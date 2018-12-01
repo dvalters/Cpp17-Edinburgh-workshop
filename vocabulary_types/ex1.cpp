0091def6517e565f859d6720efcd3998fbd354a4
Declan Valters <declan.valters@gmail.com>
2018-12-01 15:37:01 +0000

    std::optional working..sort of

--- vocabulary_types/ex1.cpp
+++ vocabulary_types/ex1.cpp
@@ -7,19 +7,28 @@
 //            return true;
 //    }
 //    return false;
+//
+std::optional<int> get_42(int i) {
+  //int i;
+  std::optional<int> result;
+  if (std::cin >> i && i == 42)
+    result = 42;
+  return result;
+  
 
-std::optional<int> get_42(int result) {
-    int i;
-    if (std::cin >> i && i == 42) {
-        result = 42;
-    }
-    return result;
+//std::optional<int> get_42(int result) {
+//    int i;
+//    if (std::cin >> i && i == 42) {
+//        result = 42;
+//    }
+//    return result;
  
 
 }
 int main() {
     int i;
-    if (get_42(i)) std::cout << i << "\n";
+    // forty_two = get_42(i);
+    if (get_42(i)) std::cout << i  << "\n";
     else std::cout << "OH NO" << "\n";
 }
 
